17ccddc6d73b085af812a23e09338cbb9bd4f166
Caoyehong1015 <154317794+Caoyehong1015@users.noreply.github.com>
2023-12-25 20:31:36 +0800

    Update selectElement.m

--- 三角形代码/selectElement.m
+++ 三角形代码/selectElement.m
@@ -1,21 +1,13 @@
 function [selection]=selectElement()
-fprintf('计算单元类型：\n');
-fprintf('CPS6--二阶三角形平面应变单元\n');
-fprintf('CPS4--线性四边形形平面应变单元\n');
-fprintf('C3D4--线性四面体单元\n');
-fprintf('S4R --缩减积分壳单元\n');
+fprintf('璁＄畻鍗曞厓绫诲瀷:\n');
+fprintf('CPS6--浜岄樁涓夎褰㈠钩闈㈠簲鍙樺崟鍏僜n');
 selection=input('\n','s');
 switch selection
     case 'CPS6'
-        fprintf('选择单元:%s\n',selection);
-    case 'CPS4'
-        fprintf('选择单元:%s\n',selection);
-    case 'C3D4'
-        fprintf('选择单元:%s\n',selection);
-    case 'S4R'
-        fprintf('选择单元:%s\n',selection);
+        fprintf('閫夋嫨鍗曞厓:%s\n',selection);
+   
     otherwise
-        fprintf('输入错误\n');
+        fprintf('杈撳叆閿欒\n');
 end
 % LOAD DATA
 selection=strcat(selection,'.inp');
